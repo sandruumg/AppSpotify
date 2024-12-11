2f8c022f1f25d0bd4110cd8467441e5f66f52301
sandruumg <sandra.munoz.garcia99@gmail.com>
2024-12-11 18:17:34 +0100

    Subida de la app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
-    api: 'http://localhost:3000/api/1.0'
-  };
-  
+  production: true,
+  api: 'https://api-2o1ky55kb-sandras-projects-b3023a25.vercel.app/api/1.0' // URL de la API en Vercel
+};
